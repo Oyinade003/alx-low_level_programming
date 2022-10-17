89979319bb02147d37a096293c8c57acdee55439
Oyinade003 <Opedamola003@gmail.com>
2022-10-17 14:34:35 -0700

    deletion

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,26 +3,28 @@
  * main - program compilation begins from main
  * Description: Program to print all possible combinations of numbers
  with no two numbers same in pairing
- * author: sammykingx
  * Return: 0 means success
  */
 int main(void)
 {
- int first, second, newline, comma, space;
+int first, second, newline, comma, space;
 newline = 10;
 comma = 44;
 space = 32;
 
-for (first = 48; first <=57; first++)
+for (first = 48; first <= 57; first++)
 {
 for (second = 49; second <= 57; second++)
- {
- if ( first != second && second > first)
- {
+{
+if ( first != second && second > first)
+{
                                 putchar((char) first);
                                 putchar((char) second);
-                                putchar((char) comma);
+if first != 56 , second != 56  
+{                          
+putchar((char) comma);
 putchar((char) space);
+}
 }
 }
 }
