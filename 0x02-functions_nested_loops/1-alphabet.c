808c684e1ff83b7626d1890fbc23ffb402d6cee5
Oyinade003 <Opedamola003@gmail.com>
2022-10-18 15:27:14 -0700

    Counting alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,13 @@
  * print_alphabet - entry point
  * Return: 0
  */
-
 void print_alphabet(void)
 {
 char c;
 int newline = 10;
-             
 for (c = 97; c <= 122; c++)
 {
 _putchar(c);
-        
 }
 _putchar((char) newline);
 }
