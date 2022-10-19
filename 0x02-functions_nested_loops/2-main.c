4c2b1012f45e3cb31a39e79f9c783181737ef396
Oyinade003 <Opedamola003@gmail.com>
2022-10-19 06:15:02 -0700

    To print alphabets

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,20 +1,41 @@
 #include "main.h"
 
+
+
 /**
+
  * print_alphabet - entry point
+
  * Return: 0
+
  */
+
 void print_alphabet_x10(void);
+
 {
+
 char c;
+
 int newline, counter;
+
 newline = 10;
 
-for (counter = 1; counter < 10; counter++) {
-for (c = 97; c <= 122; c++)
+
+
+for (counter = 1; counter < 10; counter++) 
+
 {
+
+for (c = 97; c <= 122; c++)
+
+{
+
 _putchar(c);
+
 }
-}
+
 _putchar((char) newline);
+
+}
+
 }
