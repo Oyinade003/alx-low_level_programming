9082c9f619014316ce897d393f5ab18ee0287f56
Oyinade003 <Opedamola003@gmail.com>
2022-10-20 09:46:25 -0700

    Integers

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int newline;
+newline = 10;
+    print_number(98);
+    _putchar((char) newline);
+    print_number(402);
+    _putchar((char) newline);
+    print_number(1024);
+    _putchar((char) newline);
+    print_number(0);
+    _putchar((char) newline);
+    print_number(-98);
+    _putchar((char) newline);
+    return (0);
+}
